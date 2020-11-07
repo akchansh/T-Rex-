d1a33fa8195f033bc49854c960700c0c3df5fc6e
akchansh <73519090+akchansh@users.noreply.github.com>
2020-11-07 13:19:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var ground, invisibleGround, groundImage;
 
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
-var check,die.jump
+var check,die,jump
 var score = 0;
 
 var gameOver, restart;
